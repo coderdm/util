2ffeb590edfbbb3c3aa5754619144c0945e74f92
M, Dileep (US - Bengaluru) <dm@deloitte.com>
2017-05-09 22:56:45 +0530

    Added signup 3rd screen

--- Util POC/Util POC/SignUpAddressViewController.swift
+++ Util POC/Util POC/SignUpAddressViewController.swift
@@ -1,5 +1,5 @@
 //
-//  signUpPersonalDetailsViewController.swift
+//  signUpAddressViewController.swift
 //  Util POC
 //
 //  Created by M, Dileep (US - Bengaluru) on 5/9/17.
@@ -8,35 +8,38 @@
 
 import UIKit
 
-class signUpPersonalDetailsViewController: UIViewController {
+class SignUpAddressViewController: UIViewController {
     
     // IBOutlets
-    
-    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var contentScrollView: UIScrollView!
+    @IBOutlet weak var pipeImageView: UIImageView!
+    @IBOutlet weak var contactDetailsLabel: UILabel!
+    @IBOutlet weak var phoneNumberField: UITextField!
+    @IBOutlet weak var emailAddressField: UITextField!
+    @IBOutlet weak var resAddressLabel: UILabel!
+    @IBOutlet weak var addressField: UITextField!
+    @IBOutlet weak var pincodeField: UITextField!
+    @IBOutlet weak var cityField: UITextField!
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var footerView: UIView!
-    @IBOutlet weak var firstNameField: UITextField!
-    @IBOutlet weak var lastNameField: UITextField!
-    @IBOutlet weak var dobField: UITextField!
-    @IBOutlet weak var contentScrollView: UIScrollView!
-    @IBOutlet weak var inputStackView: UIStackView!
-    
-    
-
-    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         footerView.backgroundColor = UIColor.utlSlate
-        pageControl.tintColor = UIColor.utlSunflowerYellow
         backButton.tintColor = UIColor.utlSunflowerYellow
         nextButton.tintColor = UIColor.utlSunflowerYellow
-        firstNameField.useUnderline()
-        lastNameField.useUnderline()
-        dobField.useUnderline()
+        pageControl.tintColor = UIColor.utlSunflowerYellow
+        phoneNumberField.useUnderline()
+        emailAddressField.useUnderline()
+        addressField.useUnderline()
+        pincodeField.useUnderline()
+        cityField.useUnderline()
+
     }
 
+    // MARK: - Lifecycle
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -50,18 +53,20 @@ class signUpPersonalDetailsViewController: UIViewController {
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
+    
 
-    // MARK: - IBAction
-
-    @IBAction func backButtonAction(_ sender: Any) {
+    // MARK: - IBActions
+  
+    @IBAction func backAction(_ sender: Any) {
         let _ = navigationController?.popViewController(animated: true)
     }
     
-    @IBAction func nextButtonAction(_ sender: Any) {
+    @IBAction func nextAction(_ sender: Any) {
+        
     }
     
-    
     // MARK: - Keyboard Hide/Show
+    
     func keyboardWillShow(_ notification: Notification) {
         let info:  [AnyHashable: Any]? = notification.userInfo!
         if let keyboardSize = (info?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
@@ -74,14 +79,7 @@ class signUpPersonalDetailsViewController: UIViewController {
         contentScrollView.contentInset = UIEdgeInsets.zero
         contentScrollView.scrollIndicatorInsets = UIEdgeInsets.zero
     }
-    
-}
 
-// MARK: - UITextFieldDelegate
-extension signUpPersonalDetailsViewController: UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
+
     
 }
